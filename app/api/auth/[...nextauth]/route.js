64795ee4749cddc84ddd32783fb0537688d67d49
bigmosi <kinyeramo@gmail.com>
2023-08-26 14:39:06 +0300

    connecting to google account

--- app/api/auth/[...nextauth]/route.js
+++ app/api/auth/[...nextauth]/route.js
@@ -1,6 +1,7 @@
 import NextAuth from "next-auth/next";
 import GoogleProvider from 'next-auth/providers/google';
 import { connectedToDB } from "@utils/database";
+import User from "@models/user";
 
 
 const handler = NextAuth({
@@ -10,15 +11,33 @@ const handler = NextAuth({
         clientId: process.env.GOOGLE_ID,
         clientSecret: process.env.GOOGLE_CLIENT_SECRET,
     })],
-    async session({ session }) {},
+    async session({ session }) {
+        const sessionUser = await User.findOne({
+            email: session.user.email
+        })
+
+        session.user.id = sessionUser._id.toString();
+
+        return session;
+    },
 
     async signIn({ profile }) {
         try {
             await connectedToDB();
 
             // check if a user already exists
+            const userExists = await User.findOne({
+                email: profile.email
+            });
 
             // if not, create a new user
+            if(!userExists) {
+                await User.create({
+                    email: profile.email,
+                    username: profile.name.replace(" ", "").toLowerCase(),
+                    image: profile.picture
+                })
+            }
 
             return true;
         } catch (error) {
